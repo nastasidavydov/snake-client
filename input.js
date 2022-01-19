ef5150eb08f299fce1e1d11ab6baa2a6fe2792ff
Nastasi Davydov <nastasidavydov@gmail.com>
2022-01-19 22:16:33 +0000

    Pass a conn object to be used in installInput to send data to the server

--- input.js
+++ input.js
@@ -1,6 +1,11 @@
 // setup interface to handle user input from stdin // istening for user input via the keyboard.
 
-const setupInput = function () {
+// Stores the active TCP connection object.
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -9,14 +14,14 @@ const setupInput = function () {
   const handleUserInput = function (key) {
     if (key === '\u0003') {
       process.exit();
-    } else if (key === "\033[A") {
-      process.stdout.write('Move: up')
-    } else if (key === "\033[B") {
-      process.stdout.write('Move: down')
-    } else if (key === "\033[D") {
-      process.stdout.write('Move: left')
-    } else if (key === "\033[C") {
-      process.stdout.write('Move: right')
+    } else if (key === "w") {
+      connection.write('Move: up');
+    } else if (key === "a") {
+      connection.write('Move: left');
+    } else if (key === "s") {
+      connection.write('Move: down');
+    } else if (key === "d") {
+      connection.write('Move: right');
     }
 
   };
@@ -27,3 +32,13 @@ const setupInput = function () {
 };
 
 module.exports = {setupInput};
+
+
+// } else if (key === "\033[A") {
+//   connection.write('Move: up');
+// } else if (key === "\033[B") {
+//   connection.write('Move: left');
+// } else if (key === "\033[D") {
+//   connection.write('Move: down');
+// } else if (key === "\033[C") {
+//   connection.write('Move: right');
