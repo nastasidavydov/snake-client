03f43b5e88041b657ffa7e6f89a46f6e9c255cf0
Nastasi Davydov <nastasidavydov@gmail.com>
2022-01-19 20:59:04 +0000

    Experiment with moving commands

--- client.js
+++ client.js
@@ -9,10 +9,23 @@ const connect = function () {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+
   conn.on('connect', () => {
     console.log('Successfully connected to game server'); //<-- log to me when connected
     conn.write('Name: NAS');  //<--send to server
+    
   })
+
+  // conn.on('connect', () => {
+  //   let movements = ['Move: up', 'Move: left', 'Move: down', 'Move: right']
+  //   let interval = 0;
+  //   for (let move of movements) {
+  //     setInterval(() => {
+  //       conn.write(move);
+  //     }, 50)
+  //   }
+    
+  // })
 
   conn.on('data', (data) => {
     console.log(`Message from server:  ${data}`)  //<-- code that defines what to do when an event occurs is often called an event handler.
